5f53efc7f3646ecc60d4cd24aba95ae903014f70
Diana <patricia.patri997@gmail.com>
2025-02-28 17:34:00 +0200

    Mutat toate fișierele MATLAB în folderul Laborator1+2

--- Ex3.m
+++
@@ -1,6 +0,0 @@
-syms x y
-a = 5; % Variabilă numerică
-b = x^2 + y;
-% Verificare tip
-disp(['a este simbolică? ', num2str(isa(a, 'sym'))]);
-disp(['b este simbolică? ', num2str(isa(b, 'sym'))]);
--- Ex7.m
+++
@@ -1,6 +0,0 @@
-syms x
-f = x^2 + 2*x + 1; % Definim o funcție simbolică
-% Evaluăm funcția pentru x = 3
-val = subs(f, x, 3);
-disp('Valoarea expresiei pentru x = 3 este:');
-disp(val);
